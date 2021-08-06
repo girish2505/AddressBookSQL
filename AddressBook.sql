5b37d3d12bd2185eb00601d5d3f9a1b04c935c5b
Girish Guptha Vayugundla <girish.v@tvsnext.io>
2021-08-06 12:14:39 +0530

    inserting data into address book table

--- AddressBook.sql
+++ AddressBook.sql
@@ -17,3 +17,9 @@ ZipCode int,
 Phonenum bigint,
 EmailId varchar(100)
 );
+
+----------UC3-----------
+
+Insert into Address_Book_Table values('girish','guptha','bazzar street','nellore','andhra',524406,7660094458,'v.g@gmail.com'),
+('manoj','Kumar','tidel park','chennai','tamilNadu',600119,9989912345,'mm@gmail.com');
+Insert into Address_Book_Table values('chandu','kanth','nandhalur','kadapa','andhra',523412,9848022338,'ck@gmail.com');
