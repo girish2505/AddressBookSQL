421b39cc98dc376737d022fa2a5d065d9392e010
Girish Guptha Vayugundla <girish.v@tvsnext.io>
2021-08-06 12:20:30 +0530

    update existing contact in the address book table

--- AddressBook.sql
+++ AddressBook.sql
@@ -23,3 +23,11 @@ EmailId varchar(100)
 Insert into Address_Book_Table values('girish','guptha','bazzar street','nellore','andhra',524406,7660094458,'v.g@gmail.com'),
 ('manoj','Kumar','tidel park','chennai','tamilNadu',600119,9989912345,'mm@gmail.com');
 Insert into Address_Book_Table values('chandu','kanth','nandhalur','kadapa','andhra',523412,9848022338,'ck@gmail.com');
+
+-------------UC4--------------
+
+update Address_Book_Table set Phonenum=9989999899 where Address='tidel park';
+
+update Address_Book_Table set EmailId='c.k@gmail.com' where FirstName='chandu';
+
+select * from Address_Book_Table;
